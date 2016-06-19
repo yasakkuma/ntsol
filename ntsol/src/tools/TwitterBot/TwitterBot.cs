1c082b4bb304785255f9177b498007f87a2a8a6f
yasakkuma <myahagi@hotmail.co.jp>
2016-06-19 14:15:33 +0900

    XmlReaderクラス、TwitterBotクラス ・初回登録

---
+++ ntsol/src/tools/TwitterBot/TwitterBot.cs
@@ -0,0 +1,161 @@
+﻿/* TwitterBotクラスファイル */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using CoreTweet;
+
+/* TwitterBotパッケージ名前空間 */
+namespace ntsol.Tools.TwitterBot
+{
+    /// <summary>
+    /// TwitterBotクラス
+    /// </summary>
+    public class TwitterBot
+    {
+        /// <summary>
+        /// Twitterアカウント認証用トークン。
+        /// </summary>
+        private Tokens token;
+
+        /// <summary>
+        /// コンシューマキーを取得・設定する。
+        /// </summary>
+        public string ConsumerKey
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// コンシューマシークレットを取得・設定する。
+        /// </summary>
+        public string ConsumerSecret
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// アクセストークンを取得・設定する。
+        /// </summary>
+        public string AccessToken
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// アクセストークンシークレットを取得・設定する。
+        /// </summary>
+        public string AccessTokenSecret
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// ランダム辞書ファイルパスを取得・設定する。
+        /// </summary>
+        public string RandomDicFile
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// TwitterBotクラスのコンストラクタ。
+        /// </summary>
+        /// <remarks>Botのメンバを初期化する。</remarks>
+        public TwitterBot()
+        {
+            ConsumerKey = string.Empty;
+            ConsumerSecret = string.Empty;
+            AccessToken = string.Empty;
+            AccessTokenSecret = string.Empty;
+            RandomDicFile = string.Empty;
+
+        }
+
+        /// <summary>
+        /// トークンを生成する。
+        /// </summary>
+        /// <remarks>
+        /// <para>コンシューマキー、コンシューマシークレット、</para>
+        /// <para>アクセストークン、アクセストークンシークレット設定後呼び出す。</para>
+        /// </remarks>
+        public void CreateToken()
+        {
+            // トークン生成
+            token = Tokens.Create(this.ConsumerKey,
+                this.ConsumerSecret,
+                this.AccessToken,
+                this.AccessTokenSecret);
+        }
+
+        /// <summary>
+        /// パラメータに設定した内容でツイートする。
+        /// </summary>
+        /// <param name="tweet">ポストする文字列</param>
+        /// <exception cref="InvalidOperationException">トークンが未作成の場合</exception>
+        /// <exception cref="TwitterException">ツイートが重複した場合</exception>
+        public void Post(string tweet)
+        {
+            // トークンの作成状態をチェック
+            if (token == null)
+            {
+                throw new InvalidOperationException("トークンが生成されていません。\n" + 
+                    "CreateToken()を使用してトークンを生成してください。");
+            }
+
+            // ツイート
+            token.Statuses.Update(new {status = tweet});
+        }
+
+        /// <summary>
+        /// ランダムツイートする。
+        /// </summary>
+        /// <remarks>辞書ファイルから取得した文字列をランダムにツイートする。</remarks>
+        /// <exception cref="FileNotFoundException">定義したファイルが存在しない場合。</exception>
+        /// <exception cref="ntsol.Tools.TwitterBot.Post"></exception>
+        public void RandomPost()
+        {
+            List<string> randomPostList = new List<string>();
+
+            // ファイルの存在チェック
+            if (!File.Exists(RandomDicFile))
+            {
+                throw new FileNotFoundException(
+                    string.Format("ファイル\"{0}\"が見つかりません。", RandomDicFile));
+            }
+
+            using (StreamReader reader = new StreamReader(RandomDicFile))
+            {
+                string readLine;
+                
+                // 辞書ファイルからポストする一覧を取得する。
+                while ((readLine = reader.ReadLine()) != null)
+                {
+                    // コメント行は無視
+                    if (readLine[0] == '#')
+                    {
+                        continue;
+                    }
+                    randomPostList.Add(readLine);
+                }
+            }
+
+            // 乱数値を生成し、ツイート
+            Random random = new Random();
+            Post(randomPostList[random.Next(randomPostList.Count)]);
+        }
+
+        public void ReplyPost()
+        {
+
+        }
+    }
+}
---
+++ ntsol/src/tools/XmlReader/XmlReader.cs
@@ -0,0 +1,81 @@
+﻿/* XmlReaderクラスファイル */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+/* XmlReaderパッケージ名前空間 */
+namespace ntsol.Tools.XmlReader
+{
+    /// <summary>
+    /// XML読み込みクラス
+    /// </summary>
+    public class XmlReader
+    {
+        ///<value>クラスインスタンス</value>
+        private static XmlReader instance = null;
+
+        /// <summary>
+        /// インスタンスを取得するプロパティ。
+        /// </summary>
+        public static XmlReader GetInstance
+        {
+            get
+            {
+                // シングルトンでインスタンス生成
+                if (instance == null)
+                {
+                    instance = new XmlReader();
+                }
+
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// XmlReaderクラスのコンストラクタ。
+        /// </summary>
+        /// <remarks>外部クラスからインスタンスの生成を抑制する。</remarks>
+        private XmlReader()
+        {
+        }
+
+        /// <summary>
+        /// XMLファイル読み込みメソッド
+        /// </summary>
+        /// <param name="filePath">XMLファイルパス</param>
+        /// <param name="xmlData">XMLデータ格納先のデータ型</param>
+        /// <exception cref="FileNotFoundException">読み込むXMLファイルが見つからない場合</exception>
+        /// <exception cref="InvalidOperationException">
+        /// <para>XMLファイルがUTF-8でない場合</para>
+        /// <para>XMLの要素がデータクラスと一致しない場合</para>
+        /// </exception>
+        public void Read<T>(String filePath, ref T xmlData)
+        {
+
+            // ファイルの存在チェック
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException(string.Format("ファイル\"{0}\"が見つかりません。", filePath));
+            }
+
+            try
+            {
+                // XMLファイル読み込み
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+                {
+                    // デシリアライズ
+                    XmlSerializer serializer = new XmlSerializer(xmlData.GetType());
+                    xmlData = (T)serializer.Deserialize(fileStream);
+                }
+            } catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException("データクラスの格納に失敗しました。");
+            }
+        } 
+    }
+}
